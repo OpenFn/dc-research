862595c43168558272fe9ce6047b9f0a2e139bd9
Aleksa Krolls <aleksagk@gmail.com>
2020-10-23 09:33:07 +0300

    New Uid columns

--- jobs/koboToPostgres.js
+++ jobs/koboToPostgres.js
@@ -1,6 +1,10 @@
 //Sample job using language-postgresql
 //FOR mapping assessment_site records to Postgres
-upsert('cks_pnpcity', 'qid', {
+
+
+//TODO: Create cksUid column with 'unique' constraint
+upsert('cks_pnpcity', cksUid', { 
+  cksUid: state.data.kobo._id, //use for upserts; creat
   intname: state.data.kobo.INTName, //state.data.pathToKey
   idate: state.data.kobo.Idate, 
   qid: state.data.kobo.QID //if using `id` for the upsert key, we need to map it, Dane have change to qid
@@ -9,9 +13,13 @@ upsert('cks_pnpcity', 'qid', {
 });
 
 //FOR MAPPING KOBO REPEAT GROUPS --> to insert multiple records in the DB
-insertMany('cks_pnpcity_member', state => //Different syntax for repeat groups because it returns an array
+///insertMany('cks_pnpcity_member', state => //Different syntax for repeat groups because it returns an array
+
+//TODO: Create memberUid column with 'unique' constraint
+upsertMany('cks_pnpcity_member', 'ON CONSTRAINT memberUid', state => 
   state.data.kobo.Member.map(m => ({  //state.data.GroupName... we are mapping every object `m` in the Members[] Array
-    //mid: state.data.kobo.MQID, //this syntax will not work for repeat groups because Kobo returns an array
+    memberUid: m[`Member/MQID`]+'-'+state.data.kobo._id, //concatenate MQID + Kobo Id to create new Uid
+    //mid: state.data.kobo.MQID, //this syntax will NOT work for repeat groups because Kobo returns an array
     mid: m[`Member/MQID`], //for every Member 'm', return Member/MQID here
     e22name: m[`Member/E22Name`], //for every Member 'm', return Member/E22Name
     e22age: m[`Member/E22Age`]
